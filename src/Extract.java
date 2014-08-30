48ff879331d680122d971b75cd10aac3dd45ffd3
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-08-30 21:29:04 +0200

    refactored and used stopwords to prune patterns

--- src/Extract.java
+++ src/Extract.java
@@ -6,52 +6,24 @@ import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
 import edu.stanford.nlp.trees.TypedDependency;
 import edu.stanford.nlp.util.CoreMap;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Extract {
 
-    public List<Pattern> run(String text) {
-        List<Pattern> patterns = new ArrayList<Pattern>();
+    final String stopwordsFile = "stopwords.txt";
+    List<String> stopwords;
 
-        Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, pos, parse");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        Annotation annotation = pipeline.process(text);
-        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-        for (CoreMap sentence : sentences) {
-            patterns.addAll(ExtractSentencePatterns(sentence));
+    public Extract() {
+
+        try {
+            stopwords = LoadStopwords();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return patterns;
-    }
-
-    private static HashSet<Pattern> ExtractSentencePatterns(CoreMap sentence) {
-        SemanticGraph semanticGraph = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
-
-        List<Pattern> primary = ExtractPrimaryPatterns(semanticGraph.typedDependencies());
-
-        List<Pattern> combined;
-        combined = ExtractCombinedPatterns(primary, primary);
-        combined.addAll(ExtractCombinedPatterns(combined, primary));
-        combined.addAll(ExtractCombinedPatterns(combined, primary));
-
-        return PruneCombinedPatterns(combined);
-    }
-
-    private static HashSet<Pattern> PruneCombinedPatterns(List<Pattern> combined) {
-        List<Pattern> remove = new ArrayList<Pattern>();
-
-        HashSet<Pattern> patterns = new HashSet<Pattern>(combined);
-        for (Pattern pattern : patterns) {
-            if (patterns.contains(pattern.mother) && pattern.mother.relation != Pattern.Relation.conj_and
-                && pattern.father.relation != Pattern.Relation.conj_and) {
-                remove.add(pattern.mother);
-                remove.add(pattern.father);
-            }
-        }
-        patterns.removeAll(remove);
-
-        return patterns;
     }
 
     private static List<Pattern> ExtractPrimaryPatterns(Collection<TypedDependency> tdl) {
@@ -98,6 +70,41 @@ public class Extract {
 
         return results;
     }
+
+    private HashSet<Pattern> ExtractSentencePatterns(CoreMap sentence) {
+        SemanticGraph semanticGraph = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
+
+        List<Pattern> primary = ExtractPrimaryPatterns(semanticGraph.typedDependencies());
+
+        List<Pattern> combined;
+        combined = ExtractCombinedPatterns(primary, primary);
+        combined.addAll(ExtractCombinedPatterns(combined, primary));
+        combined.addAll(ExtractCombinedPatterns(combined, primary));
+
+        return PruneCombinedPatterns(combined);
+    }
+
+    private HashSet<Pattern> PruneCombinedPatterns(List<Pattern> combined) {
+        List<Pattern> remove = new ArrayList<Pattern>();
+
+        HashSet<Pattern> patterns = new HashSet<Pattern>(combined);
+        for (Pattern pattern : patterns) {
+            if (patterns.contains(pattern.mother) && pattern.mother.relation != Pattern.Relation.conj_and
+                    && pattern.father.relation != Pattern.Relation.conj_and) {
+                remove.add(pattern.mother);
+                remove.add(pattern.father);
+            }
+
+            //remove patterns containing stopwords
+            if (stopwords.contains(pattern.head) || stopwords.contains(pattern.modifier)) {
+                remove.add(pattern);
+            }
+        }
+        patterns.removeAll(remove);
+
+        return patterns;
+    }
+
     private List<String> LoadStopwords() throws IOException {
         List<String> words = new ArrayList<String>();
 
@@ -109,5 +116,20 @@ public class Extract {
         br.close();
 
         return words;
+    }
+
+    public List<Pattern> run(String text) {
+        List<Pattern> patterns = new ArrayList<Pattern>();
+
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, parse");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        Annotation annotation = pipeline.process(text);
+        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+        for (CoreMap sentence : sentences) {
+            patterns.addAll(ExtractSentencePatterns(sentence));
+        }
+
+        return patterns;
     }
 }
