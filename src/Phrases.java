0f6d730481b965963a2198c83c92fca67f7dddc3
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-08-30 21:28:27 +0200

    added method to save to mongo

--- src/Phrases.java
+++ src/Phrases.java
@@ -1,35 +1,60 @@
 import com.mongodb.*;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Phrases {
 
     final static String MongoUri = "mongodb://localhost:27030/";
     final static String Database = "Reviews";
-    final static String Collection = "Reviews";
+    final static String Reviews = "Reviews";
+    final static String Phrases = "Phrases";
+    MongoClient mongoClient;
+    DB database;
+    DBCollection reviewsCollection, phrasesCollection;
 
-    public static void main(String[] args) {
+    public Phrases() {
         try {
-            DBCursor cursor = getDbObjects();
-            Extract extractor = new Extract();
-            try {
-                while(cursor.hasNext()) {
-                    String text = cursor.next().get("text").toString();
-                    System.out.println(extractor.run(text));
-                }
-            } finally {
-                cursor.close();
-            }
+            mongoClient = new MongoClient(new MongoClientURI(MongoUri));
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
+        database = mongoClient.getDB(Database);
+        reviewsCollection = database.getCollection(Reviews);
+        phrasesCollection = database.getCollection(Phrases);
     }
 
-    private static DBCursor getDbObjects() throws UnknownHostException {
-        MongoClient mongoClient = new MongoClient(new MongoClientURI(MongoUri));
-        DB database = mongoClient.getDB(Database);
-        DBCollection collection = database.getCollection(Collection);
+    public static void main(String[] args) {
+        Phrases phrases = new Phrases();
+        phrases.run();
+    }
 
-        return collection.find();
+    public void run() {
+        DBCursor cursor = reviewsCollection.find();
+        Extract extractor = new Extract();
+        try {
+            while (cursor.hasNext()) {
+                DBObject dbItem = cursor.next();
+                String text = dbItem.get("text").toString();
+                String reviewId = dbItem.get("reviewId").toString();
+                String business = dbItem.get("business").toString();
+
+                List<Pattern> phrases = extractor.run(text);
+                Review review = new Review(text, reviewId, business);
+                savePhrases(review, phrases);
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+
+    private void savePhrases(Review review, List<Pattern> patterns) {
+        List<String> phrases = new ArrayList<String>();
+        for (Pattern pattern : patterns) {
+            phrases.add(pattern.toAspect());
+        }
+        ReviewPhrases result = new ReviewPhrases(review, phrases);
+        phrasesCollection.insert(result.toDBObject());
     }
 }
