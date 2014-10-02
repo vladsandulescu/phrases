c1857c87050bae50469673fcc294dd2af3d63123
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-10-02 21:09:49 +0200

    make it run nicely

--- src/Phrases.java
+++ src/Phrases.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Phrases {
 
     final static String MongoUri = "mongodb://localhost:27030/";
-    final static String Database = "Reviews";
-    final static String Reviews = "Reviews";
+    final static String Database = "YelpReviews";
+    final static String Reviews = "Review";
     final static String Phrases = "Phrases";
     MongoClient mongoClient;
     DB database;
@@ -27,26 +27,42 @@ public class Phrases {
 
     public static void main(String[] args) {
         Phrases phrases = new Phrases();
-        phrases.run();
+        try {
+            phrases.run();
+        }
+        catch(Exception exception) {
+            phrases.run();
+        }
     }
 
     public void run() {
         DBCursor cursor = reviewsCollection.find();
+        int count = cursor.count();
         Extract extractor = new Extract();
+        int done = 0;
         try {
             while (cursor.hasNext()) {
                 DBObject dbItem = cursor.next();
-                String text = dbItem.get("text").toString();
-                String reviewId = dbItem.get("reviewId").toString();
-                String business = dbItem.get("business").toString();
+                String text = dbItem.get("Text").toString();
+                String reviewId = dbItem.get("ReviewId").toString();
+                String business = dbItem.get("Business").toString();
 
-                List<Pattern> phrases = extractor.run(text);
-                Review review = new Review(text, reviewId, business);
-                savePhrases(review, phrases);
+                if (!alreadyExtractedPhrases(reviewId)) {
+                    List<Pattern> phrases = extractor.run(text);
+                    Review review = new Review(text, reviewId, business);
+                    savePhrases(review, phrases);
+                }
+
+                done++;
+                System.out.println("Done " + done + "/" + count);
             }
         } finally {
             cursor.close();
         }
+    }
+
+    private boolean alreadyExtractedPhrases(String reviewId) {
+        return phrasesCollection.find(new BasicDBObject("review.reviewId", reviewId)).count() > 0;
     }
 
     private void savePhrases(Review review, List<Pattern> patterns) {
